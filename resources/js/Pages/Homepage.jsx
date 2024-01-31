d973560c3d986cdf7069771282fa43dfa564a54f
vance <rfir@gmail.com>
2024-01-31 13:20:25 +0700

    test

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -7,7 +7,8 @@ export default function Homepage (props) {
     return (
         <div className='flex justify-center items-center min-h-screen'>
             <Head title={props.title}/>
-            <h1>
+            <h1>\
+                dkaeokdeaodkoaekdeao
                 {props.description}
             </h1>
         </div>

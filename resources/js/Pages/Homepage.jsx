5ee41e380dcfd746c2d4c161ba65533953c8d2c1
vance <rfir@gmail.com>
2024-01-31 13:08:07 +0700

    first commit

---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import { Link, Head } from '@inertiajs/react';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+
+import React from 'react';
+
+export default function Homepage (props) {
+    return (
+        <div className='flex justify-center items-center min-h-screen'>
+            <Head title={props.title}/>
+            <h1>
+                {props.description}
+            </h1>
+        </div>
+    );
+}

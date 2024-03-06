6084862d5afd6225d5763cb73795c0bb702c49f2
kencs123 <kenneth.santoso@binus.ac.id>
2024-03-06 20:44:24 +0700

    confirm

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,16 +1,32 @@
-import { Link, Head } from '@inertiajs/react';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-
 import React from 'react';
+import { Head } from '@inertiajs/react';
 
-export default function Homepage (props) {
-    return (
-        <div className='flex justify-center items-center min-h-screen'>
-            <Head title={props.title}/>
-            <h1>\
-                dkaeokdeaodkoaekdeao
-                {props.description}
-            </h1>
-        </div>
-    );
+export default function Homepage(props) {
+  return (
+    <div>
+      <Head title={props.title} />
+      {props.pets ? (
+        props.pets.map((data, i) => {
+          return (
+            <div key={i}>
+              <h1>name: {data.name}</h1>
+              <p>description: {data.description}</p>
+              <p>price: {data.price}</p>
+              <p>location: {data.location}</p>
+              {data.image && (
+                <img
+                  src={`/storage/${data.image}`} 
+                  alt={data.name}
+                  style={{ maxWidth: '100%', height: '150px' }}
+                />
+              )}
+              <br />
+            </div>
+          );
+        })
+      ) : (
+        <p>No pets found.</p>
+      )}
+    </div>
+  );
 }
